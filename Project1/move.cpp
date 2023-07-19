6302f6dbc786faede02e30eac22e99b70bb95efa
Adrial <644001286@qq.com>
2023-07-19 22:35:04 +0800

    增加了新的到吗

---
+++ Project1/move.cpp
@@ -0,0 +1,198 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+
+// 顶点着色器代码
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main() {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+)";
+
+// 片段着色器代码
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+    }
+)";
+
+int main() {
+    // 初始化GLFW
+    if (!glfwInit()) {
+        std::cout << "GLFW初始化失败！" << std::endl;
+        return -1;
+    }
+
+    // 配置GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // 创建窗口
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "移动正方形", nullptr, nullptr);
+    if (!window) {
+        std::cout << "窗口创建失败！" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // 设置当前上下文
+    glfwMakeContextCurrent(window);
+
+    // 加载GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "GLAD加载失败！" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // 编译顶点着色器
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // 检查顶点着色器是否编译成功
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cout << "顶点着色器编译失败：" << infoLog << std::endl;
+    }
+
+    // 编译片段着色器
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // 检查片段着色器是否编译成功
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "片段着色器编译失败：" << infoLog << std::endl;
+    }
+
+    // 创建着色器程序
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // 检查着色器程序是否链接成功
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "着色器程序链接失败：" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // 设置正方形的顶点数据
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f,
+    };
+
+    // 设置正方形的索引数据
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    // 创建顶点缓冲对象和索引缓冲对象
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // 绑定顶点数组对象和缓冲对象
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // 设置顶点属性指针
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // 解绑VAO和缓冲对象
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // 在程序的全局作用域定义一个偏移量
+    float offsetX = 0.0f;
+    float offsetY = 0.0f;
+
+    // 渲染循环
+    while (!glfwWindowShouldClose(window)) {
+        // 输入处理
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        // 更新偏移量，控制移动速度
+        float speed = 0.00005f;
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+            offsetY += speed;
+        }
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+            offsetY -= speed;
+        }
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+            offsetX -= speed;
+        }
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+            offsetX += speed;
+        }
+
+        // 渲染指令
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+
+        // 更新顶点数据，根据偏移量移动正方形
+        float updatedVertices[] = {
+            -0.5f + offsetX, -0.5f + offsetY, 0.0f,
+             0.5f + offsetX, -0.5f + offsetY, 0.0f,
+             0.5f + offsetX,  0.5f + offsetY, 0.0f,
+            -0.5f + offsetX,  0.5f + offsetY, 0.0f,
+        };
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(updatedVertices), updatedVertices, GL_STATIC_DRAW);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // 交换缓冲区和获取事件
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // 清除分配的资源
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    // 终止GLFW
+    glfwTerminate();
+    return 0;
+}
+
+
